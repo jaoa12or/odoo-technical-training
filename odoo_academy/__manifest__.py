70529659f6fa7cdad336d60491679de20a77f861
Alejandro Orobio <jhon.orobio@correounivalle.edu.co>
2022-09-21 22:19:21 -0500

    Add: new module base

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'sumary': """Academy app to manage Training""",
+    'description': """
+        Academy Moule to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'author': 'Rude Coding',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+}
