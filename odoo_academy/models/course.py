022f413854f30922ccd8b8818ee9ac53d3cd4c38
Alejandro Orobio <jhon.orobio@correounivalle.edu.co>
2022-09-27 22:42:23 -0500

    Add: fields

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,21 @@
 from odoo import api, fields, models
 
+
 class Course(models.Model):
-    _name = 'academy.course'
-    _description = 'Course Info'
-    
+    _name = "academy.course"
+    _description = "Course Info"
+
+    name = fields.Char("Title", required=True)
+    description = fields.Text(string="Description")
+
+    level = fields.Selection(
+        [
+            ("beginner", "Beginner"),
+            ("intermediate", "Intermediate"),
+            ("advanced", "Advanced"),
+        ],
+        string="Level",
+        copy=False,
+    )
+
+    active = fields.Boolean('Active', default=True)
